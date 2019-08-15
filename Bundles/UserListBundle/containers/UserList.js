825a54a8f3c6d81113c5ea27b411635ba17e4ac4
François-Luc Haghenbeek <francois-luchaghenbeek@MacBook-Pro-de-Francois-Luc.local>
2019-08-15 20:21:12 +0200

    Filter users when rendering to avoid state redefinition.

--- Bundles/UserListBundle/containers/UserList.js
+++ Bundles/UserListBundle/containers/UserList.js
@@ -29,25 +29,33 @@ class UserList extends React.Component {
     const { users } = this.state
     this.setState({
       name: value,
-      users: users.filter(
-        (user) =>
-          user.last_name.startsWith(value) || user.first_name.startsWith(value)
-      ),
     })
   }
 
   render() {
     const { users, name } = this.state
+    console.log()
     return (
       <View>
-        <InputItem value={name} onChange={this.handleChange} />
+        <InputItem
+          value={name}
+          onChange={this.handleChange}
+          placeholder='Rechercher un utilisateur'
+        />
         <ScrollView>
           <AntList>
-            {name.length >= 3 && users.map((user) => (
-              <AntList.Item key={user.id}>
-                {user.first_name} {user.last_name}
-              </AntList.Item>
-            ))}
+            {name.length >= 3 &&
+              users
+                .filter(
+                  (user) =>
+                    user.last_name.startsWith(name) ||
+                    user.first_name.startsWith(name)
+                )
+                .map((user) => (
+                  <AntList.Item key={user.id}>
+                    {user.first_name} {user.last_name}
+                  </AntList.Item>
+                ))}
           </AntList>
         </ScrollView>
       </View>
